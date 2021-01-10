80866af887a71394799705c740e7d55a162b36d0
Giedrius Naprys <giedrius.naprys@gmail.com>
2021-01-10 18:56:32 +0200

    1stStage done

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Rooms() {
     return (
         <div>
-            hello from home
-        </div>
+            hello from Rooms        </div>
     )
 }
